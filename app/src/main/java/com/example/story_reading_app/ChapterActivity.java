22689d6694cb61103d8a447b180a3077af357fa1
APIWebService <dotrongthuong8@gmail.com>
2022-01-03 11:31:00 +0700

    finish detail story

---
+++ app/src/main/java/com/example/story_reading_app/ChapterActivity.java
@@ -0,0 +1,14 @@
+package com.example.story_reading_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter);
+    }
+}
--- app/src/main/java/com/example/story_reading_app/StoryDetailActivity.java
+++ app/src/main/java/com/example/story_reading_app/StoryDetailActivity.java
@@ -1,15 +1,38 @@
 package com.example.story_reading_app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.lib.model.StoryModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class StoryDetailActivity extends AppCompatActivity {
 
@@ -18,6 +41,7 @@ public class StoryDetailActivity extends AppCompatActivity {
     TextView txtAuthorDetail;
     TextView txtCateStoryDetail;
     TextView txtStatusDetail;
+    TextView txtSummaryDetail;
     RelativeLayout relativeBackground;
 
     @Override
@@ -30,6 +54,7 @@ public class StoryDetailActivity extends AppCompatActivity {
         txtAuthorDetail = findViewById(R.id.txtAuthorDetail);
         txtCateStoryDetail = findViewById(R.id.txtCateStoryDetail);
         txtStatusDetail = findViewById(R.id.txtStatusDetail);
+        txtSummaryDetail = findViewById(R.id.txtSummaryDetail);
         relativeBackground = findViewById(R.id.relativeBackground);
 
         Intent intent = getIntent();
@@ -39,8 +64,30 @@ public class StoryDetailActivity extends AppCompatActivity {
         txtAuthorDetail.setText(model.getAuthor());
         txtCateStoryDetail.setText(model.getCategoryName());
         txtStatusDetail.setText(model.getStatusName());
+        txtSummaryDetail.setText(model.getSummaryContent());
         Glide.with(this).load(model.getImage()).into(imgStoryDetail);
-        relativeBackground.setBackground(Glide.with(this).load(model.getImage()).into(imgStoryDetail).getView().getBackground());
+
+        Glide.with(this)
+                .load(model.getImage())
+                .into(new CustomTarget<Drawable>() {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        relativeBackground.setBackground(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
+    }
+
+    public void goToBackHome(View view) {
+        finish();
+    }
+
+    public void goToChapter(View view) {
 
     }
 }
